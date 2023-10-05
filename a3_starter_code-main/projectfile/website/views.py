de417f8910d315ec2d5d5ab41d9c9c75054d8667
RickBurf <patburford00@gmail.com>
2023-10-05 14:48:05 +1000

    qwqw

--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -7,6 +7,19 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@bp.route('/create')
+def create():
+    return render_template('create.html')
+
+@bp.route('/history')
+def history():
+    return render_template('history.html')
+
+@bp.route('/sevents')
+def sevents():
+    return render_template('SEVENTS.html')
+
+
 @bp.route('/search', methods=['GET', 'POST'])
 def search():
     # Your search logic here

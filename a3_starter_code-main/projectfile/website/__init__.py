67ccc6733d24fd671779f5e44d17139011f4a9eb
RickBurf <138586223+RickBurf@users.noreply.github.com>
2023-10-15 15:00:18 +1000

    Update __init__.py

--- a3_starter_code-main/projectfile/website/__init__.py
+++ a3_starter_code-main/projectfile/website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -11,9 +12,24 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydbname.sqlite'
     db.init_app(app)
     bootstrap = Bootstrap(app)
-    
-    # Import your views module here to avoid circular references
+
+    #initialise the login manager
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    #create a user loader function takes userid and returns User
+    from .models import User  # importing here to avoid circular references
+    @login_manager.user_loader
+    def load_user(user_id):
+      return User.query.get(int(user_id))
+
+    # Import and register your views module here
     from . import views
     app.register_blueprint(views.bp)
+
+    # Import and register the auth blueprint here
+    from . import auth
+    app.register_blueprint(auth.authbp)
 
     return app
